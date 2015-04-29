cdf4a0afd8bb0adefe624040c7f9de83b9d81c93
angelomiranda <angelo.miranda@live.com>
2015-04-28 21:32:57 -0400

    Add styles task

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
 
+'use strict';
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
 var argv = require('yargs').argv;
 var browserify = require('gulp-browserify');
 var buffer = require('vinyl-buffer');
-var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
-var plumber = require('gulp-plumber');
-var source = require('vinyl-source-stream');
-var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 
 var paths = {
   entrySrc: './assets/js/*.js',
+  entrySCSS: './assets/sass/*.scss',
   scriptSrc: './assets/js/*.js',
-  buildScriptDir: './public/js/'
+  buildScriptDir: './public/js/',
+  buildCSSDir: './public/css/'
 };
 
 gulp.task('jshint', function() {
@@ -33,9 +34,13 @@ gulp.task('scripts', function() {
             throw err;
         })
         .pipe(buffer())
-        // .pipe(sourcemaps.init({loadMaps: true}))
-        // .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.buildScriptDir));
+});
+
+gulp.task('styles', function() {
+    gulp.src(paths.entrySCSS)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.buildCSSDir));
 });
 
 gulp.task('default', ['jshint', 'scripts']);
