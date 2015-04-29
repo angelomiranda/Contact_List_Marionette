499bfc8493f93cdca07176827559b02f72156ca6
angelomiranda <angelo.miranda@live.com>
2015-04-28 21:17:07 -0400

    Remove comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
-
-'use strict';
+/*jshint unused: true */
 
 var $ = require('jquery');
 
@@ -9,10 +8,7 @@ var ContactListView = require('./src/views/contact-item-view');
 var AddContactView = require('./src/views/add-contact-view');
 
 $(document).ready(function() {
-
-    $('#js-modalLauncher').on('click', function () {
-        $('#js-name').focus();
-    });
+    'use strict';
 
     var contactCollection = new ContactCollection();
 
@@ -29,29 +25,3 @@ $(document).ready(function() {
     contactListView.render();
 
 });
-
-// // Get All
-// $.get('/contacts')
-//     .then(function(response){console.log(response)})
-
-// // Get By ID
-// $.get('/contacts/id')
-//     .then(function(response){console.log(response)});
-
-// // Add a Contact
-// $.post('/contacts', {name: 'JhunAvi', age: 4, gender: 'Male'})
-//     .then(function (data) {
-//         console.log(data)
-//     });
-
-// // Delete a Contact
-// $.post('/contact/552c7e1ed6055d182232b1f0', { _method : 'delete' })
-//     .then(function(response) {
-//         console.log(response);
-//     });
-
-// // Update a contact by ID
-// $.post('/contact/552b059b76d9f1deca021adf', { _method : 'put', name: 'Angelo', age: 34, gender: 'Male' })
-//     .then(function(response) {
-//         console.log(response);
-//     });
